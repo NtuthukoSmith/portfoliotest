318fae933c70e8e84df04d3797f7e5365a5bd0ab
AnonyRex <AnonyRex101@gmail.com>
2023-09-19 21:11:38 +0200

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// template_qe55pva
+// service_mvxpgsn
+// Tf6Utke54AFEH7h2N
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -31,18 +35,24 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
+
+    // template_qe55pva
+// service_mvxpgsn
+// Tf6Utke54AFEH7h2N
+
+
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_mvxpgsn',
+        'template_qe55pva',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Ntuthuko Smith",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "ntuthukosmithsa@outlook.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'Tf6Utke54AFEH7h2N'
       )
       .then(
         () => {
